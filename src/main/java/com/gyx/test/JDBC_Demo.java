7c8421704cd5ca80332b8fd470a095b02061f98f
gyx <1604059807@qq.com>
2022-12-02 22:23:49 +0800

    second commit

--- src/main/java/com/gyx/test/JDBC_Demo.java
+++ src/main/java/com/gyx/test/JDBC_Demo.java
@@ -9,29 +9,29 @@ import java.util.List;
 
 public class JDBC_Demo {
     public static void main(String[] args) throws Exception{
-        //注册驱动
+        //1.注册驱动
         Class.forName("org.postgresql.Driver");
 
-        //获取连接
+        //2.获取连接
         String url="jdbc:postgresql://123.249.71.167:26000/demo";
         String username="joe";
         String password="Bigdata@123";
         Connection connection = DriverManager.getConnection(url, username, password);
         System.out.println("数据库连接成功!");
 
-        //定义sql
+        //3.定义sql
         String sql="select * from books";
 
-        //获取sql执行对象
+        //4.获取sql执行对象
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
-        //执行sql
+        //5.执行sql
         ResultSet resultSet = preparedStatement.executeQuery();
 
-        //创建集合
+        //6.创建集合
         List<Book> list=new ArrayList<>();
 
-        //显示结果
+        //7.显示结果
         while (resultSet.next()){
             Book book = new Book();
             String bid=resultSet.getString("bid");
@@ -46,7 +46,7 @@ public class JDBC_Demo {
         }
         System.out.println(list);
 
-        //释放资源
+        //8.释放资源
         connection.close();
 
     }
